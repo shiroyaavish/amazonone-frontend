2fe877e11c669e164480b003d47b89288ead2e95
shiroyaavish <avishshiroya2034@gmail.com>
2025-11-23 12:48:02 +0530

    fix  : build error

--- app/popular/page.tsx
+++ app/popular/page.tsx
@@ -1,7 +1,7 @@
-import ProductDetailPage from "@/components/ProductDetailPage";
 import ProductFilter from "@/components/ProductFilter";
-import { apiHelpers } from "@/lib/axios";
+import { Loader2 } from "lucide-react";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export async function generateMetadata(): Promise<Metadata> {
     return {
@@ -23,5 +23,15 @@ export async function generateMetadata(): Promise<Metadata> {
     };
 }
 export default async function PopularPage() {
-    return <ProductFilter />;
+    return (<div className="min-h-screen bg-gray-50">
+        <Suspense
+            fallback={
+                <div className="flex items-center justify-center py-16">
+                    <Loader2 className="w-10 h-10 text-green-600 animate-spin" />
+                </div>
+            }
+        >
+            <ProductFilter />
+        </Suspense>
+    </div>)
 }
